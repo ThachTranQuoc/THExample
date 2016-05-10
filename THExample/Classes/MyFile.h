9fb84858a78c8f2684df82f608820446febc5e00
OnWorldTV <onworldtv@OnWorldTVs-Mac-mini.local>
2016-05-10 16:41:03 +0700

    [ADD]project

---
+++ THExample/Classes/MyFile.h
@@ -0,0 +1,14 @@
+//
+//  MyFile.h
+//  Asia
+//
+//  Created by OnWorldTV on 5/10/16.
+//  Copyright © 2016 Onworld. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MyFile : NSObject
+
+@property (nonatomic)NSString * myProperty;
+@end
---
+++ THExample/Classes/MyFile.m
@@ -0,0 +1,13 @@
+//
+//  MyFile.m
+//  Asia
+//
+//  Created by OnWorldTV on 5/10/16.
+//  Copyright © 2016 Onworld. All rights reserved.
+//
+
+#import "MyFile.h"
+
+@implementation MyFile
+
+@end
